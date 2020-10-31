6d66a54a40feb13c98e234a77d9f282bb1855e75
eowsley <66394238+eowsley@users.noreply.github.com>
2020-10-30 23:11:02 -0700

    need help solving

---
+++ app.js
@@ -0,0 +1,4 @@
+// Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
+const form = document.querySelector('form');
+
+// need help solving this one
